02a09bc0444b0a6141a7bcfb12da70ae3cb5dc5e
winton <ywd979@foxmail.com>
2017-11-15 14:47:57 +0800

    高效单例模式

---
+++ src/main/java/singleton/Demo.java
@@ -0,0 +1,12 @@
+package singleton;
+
+/**
+ * Created by winton on 2017/11/15.
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        SingletonE.INSTANCE.sinnletonSay();
+    }
+
+}
---
+++ src/main/java/singleton/SingletonE.java
@@ -0,0 +1,14 @@
+package singleton;
+
+/**
+ * Created by winton on 2017/11/15.
+ */
+public enum SingletonE {
+
+    INSTANCE;
+
+    public void sinnletonSay() {
+        System.out.println("hello");
+    }
+
+}
