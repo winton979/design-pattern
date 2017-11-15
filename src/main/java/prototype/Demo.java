0e2e41f99e09c4b4f07cbe2825fd4e97861c719e
winton <ywd979@foxmail.com>
2017-11-15 14:40:02 +0800

    原型模式

--- src/main/java/prototype/Demo.java
+++ src/main/java/prototype/Demo.java
@@ -7,7 +7,10 @@ public class Demo {
     public static void main(String[] args) {
         Toyota t = (Toyota) CarPrototype.getCar("toyota");
         Toyota t2 = (Toyota) CarPrototype.getCar("toyota");
+        Benz b = (Benz) CarPrototype.getCar("benz");
         System.out.println(t);
         System.out.println(t2);
+        t.run();
+        b.run();
     }
 }
